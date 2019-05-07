31ac727e5a48eef65b0fdeb98a1ff287f72a635d
Mikkel <mikk4211@edu.easj.dk>
2019-05-07 18:02:54 +0200

    Sidste touch, kommentarer osv.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main{
 
     public static void main(String[] args) {
@@ -5,10 +7,15 @@ public class Main{
         Truck truck = new Truck(12000);
         System.out.println(car.vehicleWeight);
         System.out.println(truck.loadWeight);*/
+
+        // Laver ny vehiclegenerator og definerer hvor mange biler den skal lave, og printer dem herefter for at kunne sammenligne dem.
+
         VehicleGenerator vehicleGenerator = new VehicleGenerator();
         System.out.println(vehicleGenerator.vehicleGenerator(100));
-        Car car1 = new Car(600);
+        
+        Car car1 = new Car(1100);
         Car car2 = new Car(600);
+
 
         System.out.println(car1.compareTo(car2));
         System.out.println(car2.vehicleWeight);
